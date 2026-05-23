23bf642c51f45300f58896f8e99f9808ce1df658
Megan Bowen <meganbowen@Megans-MacBook-Pro-2.local>
2026-05-23 17:45:29 -0400

    Remove dead src/components/routines/ directory (33 files)

--- src/components/routines/player/index.ts
+++
@@ -1,6 +0,0 @@
-// Export components
-export { default as RoutinePlayer } from './RoutinePlayer';
-export { default as CountdownTimer } from './components/CountdownTimer';
-
-// Export store from main store directory
-export { useRoutinePlayerStore } from '@/store/routinePlayerStore';
